a3a7ac323553b63b1529d3049362dd11c71b05ab
Kazuki Ono <kazuki.1209.haru@gmail.com>
2019-11-14 00:42:49 +0900

    'キャッシュについて'

---
+++ calc-prop-cache/index.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+  el: '#app',
+  computed: {
+    computedNumber: function() {
+      console.log('computed!')
+      return Math.random()
+    }
+  },
+  methods: {
+    methodsNumber: function() {
+      console.log('methods')
+      return Math.random()
+    }
+  }
+
+})
