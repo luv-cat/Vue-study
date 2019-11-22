88a1976ab0578dfd3d67561f97da86172c119d39
Kazuki Ono <kazuki.1209.haru@gmail.com>
2019-11-23 00:07:54 +0900

    イベントハンドラに引数を渡す

---
+++ eventHandler/index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    counter: 0,
+    message: ''
+  },
+  methods: {
+    clickHandler: function (message) {
+      this.message = message
+      this.counter++
+      console.log(message);
+    }
+  }
+})
