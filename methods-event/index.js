c4433e1a6c06a90fa27d814af7e05232986104d6
Kazuki Ono <kazuki.1209.haru@gmail.com>
2019-11-22 23:52:27 +0900

    メソッドハンドラとイベンドオブジェクトの参照

---
+++ methods-event/index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    counter: 0
+  },
+  methods: {
+    clickHandler: function (event) {
+      this.counter++
+      console.log(event);
+      console.log(event.target.tagName);
+      console.log(event.target.innerHTML);
+    }
+  }
+})
